0474caa95d6873ebca6b34fc3e5a9a335ff22d79
ElperdonXV <davidemartinografico@gmail.com>
2022-02-17 16:58:31 +0100

    little design bonus done

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,13 +1,17 @@
 @extends('layouts.base')
 
 @section('content')
+<div class="container">
    @foreach ($movies as $movie)
        <div class="card">
-           <h2>{{ $movie->title }}</h2>
-           <h3>{{ $movie->original_title }}</h3>
-           <h4>{{ $movie->date }}</h4>
-           <span>{{ $movie->vote }}</span>
-           <span>{{ $movie->nationality }}</span>
-       </div>
+           <div class="title">
+                <h2>{{ $movie->title }}</h2>
+           </div>
+           <h3>Titolo originale: {{ $movie->original_title }}</h3>
+           <h4>Data: {{ $movie->date }}</h4>
+           <span>Voto: {{ $movie->vote }}</span>
+           <span>Nazionalità: {{ $movie->nationality }}</span>
+       </div>  
    @endforeach
+</div>    
 @endsection
